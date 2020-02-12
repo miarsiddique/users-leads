86db1d831b80474415a83fe9212258639d4d0619
jyoti-aika <irshad.alam@logiciesolutions.co.in>
2020-02-12 14:26:38 +0530

    collective added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => Hash::make('password'),
-        'status' => Arr::random(config('leads.status'), 1),
+        'status' => Arr::random(['ACTIVE', 'INACTIVE'], 2),
         'remember_token' => Str::random(10),
     ];
 });
